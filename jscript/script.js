5164f32ffd8fba131cbee2ec1bbd5e6dc697df25
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-01 18:00:39 +0200

    added html, simple style and preparatory phase in script

--- jscript/script.js
+++ jscript/script.js
@@ -9,7 +9,8 @@ di offerta applicata e nome passeggero
 4. dichiara una const discountO65 con lo sconto
 5. recupera i km dall'utente con l'input #kmTot e li inserisce in const
 kmTot
-6. recupera dall'utente con l'input #age l'età e la inserisce in const age
+6. recupera dall'utente con l'input #age l'età e la inserisce in const
+age
 7. recupera il nome con input #fullName e lo inserisce in const fullName
 8. calcola il prezzo del biglietto ticketPrice moltiplicando kmTot
 per costKm 
@@ -28,5 +29,19 @@ stampa il relativo sconto
 */
 
 // Fase preparatoria
+// 1.
+const tdTicketCost = document.querySelector('.td-ticket-cost');
+const tdDiscount = document.querySelector('.td-discount');
+const tdFullName = document.querySelector('.td-full-name');
+console.log('td per costo biglietto, tipo di offerta e nome completo', tdTicketCost, tdDiscount, tdFullName)
+// 2.
+const costKm = 0.21; //euro al km
+console.log('costKm', costKm);
+// 3.
+const discountU18 = 0.2;
+console.log('discountU18', discountU18);
+// 4.
+const discountO65 = 0.4;
+console.log('discountO65', discountO65);
 
  
